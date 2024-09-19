11d0bad991cb53601a1c5e20b24a23bcbfae3341
Shani Thalappully Preman <shanitpreman@gmail.com>
2024-09-19 12:41:57 -0400

    favicon

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,7 +59,7 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Portfolio | Sushith";
+            document.title = "Portfolio | Shani";
             $("#favicon").attr("href", "assets/images/favicon.png");
         }
         else {
